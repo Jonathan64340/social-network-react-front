d7c5f4f7b999205fdfd8e9d032ea10994e6978e6
Jonathan Domingues <spictor64340@gmail.com>
2022-02-10 18:09:05 +0100

    fix condition moment

--- src/_helper/moment_custom.js
+++ src/_helper/moment_custom.js
@@ -4,14 +4,13 @@ import i18n from '../i18n';
 
 const momentCustom = ({ date, locale = i18n.language, format = 'DD/MM/YYYY HH:mm', fromNowDisplay = false }) => {
     moment.locale(locale);
-    let _moment = moment(date);
-    let _date = new Date(new Date().getTime() + 60 * 60 * 24 * 1000).getTime();
+    let latest = moment(date);
 
-    if (date < _date && fromNowDisplay) {
-        return _moment.fromNow();
+    if (date > new Date().getTime() - 60 * 60 * 24 * 1000 && fromNowDisplay) {
+        return latest.fromNow();
     }
 
-    return _moment.format(format);
+    return latest.format(format);
 }
 
 export { momentCustom };
